25dbce37e4f4975574415262e4b995e7dce81706
Peder Sundbø <peder.sundbo@inmeta.no>
2018-02-08 19:22:21 +0100

    Added post tenant func

---
+++ RavenAPI/RavenAPI/Controllers/MessagesController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.Azure.Documents.Client;
+using Newtonsoft.Json;
+using RavenAPI.Helpers;
+
+namespace RavenAPI.Controllers
+{
+    public class Message
+    {
+        public string content { get; set; }
+        public String messageid { get; set; }
+    }
+
+    public class MessagesController : ApiController
+    {
+        private DocumentClient client;        
+
+        // GET: api/Messages
+        public IQueryable<Message> Get()
+        {
+            this.client = new DocumentClient(new Uri("https://ravendb.documents.azure.com:443/"), AuthHelper.CosmosKey);
+            FeedOptions queryOptions = new FeedOptions { MaxItemCount = -1 };
+
+            // Here we find the Andersen family via its LastName
+            IQueryable<Message> messageQuery = this.client.CreateDocumentQuery<Message>(
+                UriFactory.CreateDocumentCollectionUri("RavenCollection", "Messages"),
+                "SELECT * FROM Messages",
+                queryOptions);
+
+            return messageQuery;
+        }
+
+        // GET: api/Messages/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Messages
+        public object Post([FromBody] Message postcontent)
+        {
+            var guid = Guid.NewGuid().ToString();
+            this.client = new DocumentClient(new Uri("https://ravendb.documents.azure.com:443/"), AuthHelper.CosmosKey);
+
+            return Request.CreateResponse(HttpStatusCode.OK,
+               client.CreateDocumentAsync(
+                UriFactory.CreateDocumentCollectionUri("RavenCollection", "Messages"),
+                new Message
+                {
+                    content = postcontent.content,
+                    messageid = guid
+                })
+           );
+        }
+
+        // PUT: api/Messages/5
+        public void Put(int id, [FromBody]string value)
+        {
+
+        }
+
+        // DELETE: api/Messages/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ RavenAPI/RavenAPI/Controllers/TenantsController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.Azure.Documents.Client;
+using Newtonsoft.Json;
+using RavenAPI.Helpers;
+
+namespace RavenAPI.Controllers
+{
+    public class Tenant
+    {
+        public string title { get; set; }        
+    }
+
+    public class TenantsController : ApiController
+    {
+        private DocumentClient client;
+
+        // GET: api/Messages
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public object Post([FromBody] Tenant postcontent)
+        {            
+            this.client = new DocumentClient(new Uri("https://ravendb.documents.azure.com:443/"), AuthHelper.CosmosKey);
+
+            return Request.CreateResponse(HttpStatusCode.OK,
+               client.CreateDocumentAsync(
+                UriFactory.CreateDocumentCollectionUri("RavenCollection", "Tenants"),
+                new Tenant
+                {
+                    title = postcontent.title                    
+                })
+           );
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
