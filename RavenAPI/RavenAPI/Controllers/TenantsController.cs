0b4a524f7d4ca7c82b959be0d442723411b67510
Peder Sundbø <peder.sundbo@inmeta.no>
2018-02-09 19:01:56 +0100

    updates

--- RavenAPI/RavenAPI/Controllers/TenantsController.cs
+++ RavenAPI/RavenAPI/Controllers/TenantsController.cs
@@ -30,23 +30,27 @@ namespace RavenAPI.Controllers
 
         public IQueryable<Tenant> Get(string tenant)
         {
-            var query = string.Format("SELECT * FROM c WHERE c.tenantTitle", tenant);
+            var query = string.Format("SELECT * FROM c WHERE c.tenantTitle='{0}'", tenant);
             this.client = new DocumentClient(new Uri("https://ravendb.documents.azure.com:443/"), AuthHelper.CosmosKey);
             FeedOptions queryOptions = new FeedOptions { MaxItemCount = -1 };
             IQueryable<Tenant> tenantQuery = this.client.CreateDocumentQuery<Tenant>(UriFactory.CreateDocumentCollectionUri("RavenCollection", "Tenants"), query, queryOptions);
             return tenantQuery;
-        }
+        }        
 
-        public object Post([FromBody] Tenant postcontent)
+        public string Post([FromBody] Tenant postcontent)
         {
+            var guid = Guid.NewGuid().ToString().Replace("-", "");
             this.client = new DocumentClient(new Uri("https://ravendb.documents.azure.com:443/"), AuthHelper.CosmosKey);
-            return Request.CreateResponse(HttpStatusCode.OK,
-                client.CreateDocumentAsync(UriFactory.CreateDocumentCollectionUri("RavenCollection", "Tenants"),
-                new Tenant {
+            client.CreateDocumentAsync(
+                UriFactory.CreateDocumentCollectionUri("RavenCollection", "Tenants"),
+                new Tenant
+                {
                     tenantTitle = postcontent.tenantTitle,
-                    tenantId = postcontent.tenantId,
+                    tenantId = guid,
                     tenantName = postcontent.tenantName
-                }));
+                });
+
+            return guid;
         }
     }
 }
