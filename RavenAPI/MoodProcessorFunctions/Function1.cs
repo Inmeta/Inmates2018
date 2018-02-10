4b510dc46f0d14f8f3f2c5bc548275a1f9b6ee33
Bjørn Hasselberg <Bjorn.Hasselberg@inmeta.no>
2018-02-10 14:03:54 +0100

    Version adjustment

--- RavenAPI/MoodProcessorFunctions/Function1.cs
+++ RavenAPI/MoodProcessorFunctions/Function1.cs
@@ -3,10 +3,6 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.Azure.WebJobs.Host;
-using Newtonsoft.Json;
 
 namespace MoodProcessorFunctions
 {
---
+++ RavenAPI/RavenDbLib/Helpers/AuthHelper.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.KeyVault;
+
+
+namespace RavenDatabase.Helpers
+{
+    public static class AuthHelper
+    {
+        //this is an optional property to hold the secret after it is retrieved
+        public static string EncryptSecret { get; set; }
+        public static string CosmosConnectionString { get; set; }
+        public static string CosmosKey {
+            get
+            {
+                    // I put my GetToken method in a Utils class. Change for wherever you placed your method.
+                    var kv = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(AuthHelper.GetToken));
+                    var cosmosConnectionString = kv.GetSecretAsync("https://ravendbkeys.vault.azure.net/secrets/raven-cosmosdb-connectionstring/8d5b00b1614340c0a7c03601899c891a").GetAwaiter().GetResult();
+                    var cosmosKey = kv.GetSecretAsync("https://ravendbkeys.vault.azure.net/secrets/raven-cosmosdb-key/783fbe4669404366bf06c083ba9db2be").GetAwaiter().GetResult();
+                    //I put a variable in a Utils class to hold the secret for general  application use.
+                    AuthHelper.CosmosConnectionString = cosmosConnectionString.Value;
+                    //AuthHelper.CosmosKey = cosmosKey.Value;
+                return cosmosKey.Value;
+            }
+            set { }
+        }
+
+        //the method that will be provided to the KeyVaultClient
+        public static async Task<string> GetToken(string authority, string resource, string scope)
+        {
+            var authContext = new AuthenticationContext(authority);
+            ClientCredential clientCred = new ClientCredential(
+                "7b81e2a4-2637-4a8f-ba01-2c5ec382d4e8",
+                "SSm55d7hBpiwSXtdrzIFip0H9HtkMxcigaOGXfB5aAI=");
+            AuthenticationResult result = await authContext.AcquireTokenAsync(resource, clientCred);
+
+            if (result == null)
+                throw new InvalidOperationException("Failed to obtain the JWT token");
+
+            return result.AccessToken;
+        }
+    }
+}
